b959c3d8cc17d05cc5ab55e278422445ac5164b0
hsandid <hxa04@mail.aub.edu>
2020-02-28 13:38:38 +0200

    Made changes to integration tests

--- Aub.Eece503e.ChatService.Client/ProfileServiceClient.cs
+++ Aub.Eece503e.ChatService.Client/ProfileServiceClient.cs
@@ -3,7 +3,6 @@ using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Aub.Eece503e.ChatService.Datacontracts;
-using System.Collections.Generic;
 
 namespace Aub.Eece503e.ChatService.Client
 {
