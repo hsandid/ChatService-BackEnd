cfb94c3d137b77a69cdf7d497c616ebb69c704b9
hsandid <hadisandid@gmail.com>
2020-08-15 06:14:55 +0300

    Update restoreDependencies.sh

--- restoreDependencies.sh
+++ restoreDependencies.sh
@@ -4,4 +4,12 @@ dotnet restore ChatService.Datacontracts/ChatService.Datacontracts.csproj
 dotnet restore ChatService.DeploymentTests/ChatService.DeploymentTests.csproj
 dotnet restore ChatService.IntegrationTests/ChatService.IntegrationTests.csproj
 dotnet restore ChatService.Web/ChatService.Web.csproj
-dotnet restore ChatService.Tests/ChatService.Tests.csproj     
+dotnet restore ChatService.Tests/ChatService.Tests.csproj
+
+  
+dotnet build ChatService.Client/ChatService.Client.csproj --configuration Release --no-restore
+dotnet build ChatService.Datacontracts/ChatService.Datacontracts.csproj --configuration Release --no-restore
+dotnet build ChatService.DeploymentTests/ChatService.DeploymentTests.csproj --configuration Release --no-restore
+dotnet build ChatService.IntegrationTests/ChatService.IntegrationTests.csproj --configuration Release --no-restore
+dotnet build ChatService.Web/ChatService.Web.csproj --configuration Release --no-restore
+dotnet build ChatService.Tests/ChatService.Tests.csproj --configuration Release --no-restore      
